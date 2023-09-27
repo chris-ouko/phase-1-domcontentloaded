71f3a5c1ad07bdfd5d9cb9d2520265a49b12a643
+ <+>
2023-09-27 15:06:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  
+    // Target the paragraph element with id="text"
+    const textParagraph = document.getElementById("text");
+  
+    // Replace the text content
+    textParagraph.textContent = "This is really cool!";
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+    
